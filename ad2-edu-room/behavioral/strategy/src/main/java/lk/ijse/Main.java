cad175a1a5d046e3529939834f4bcd76022dd5d9
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-05-09 11:28:41 +0530

    add strategy design pattern

---
+++ ad2-edu-room/behavioral/strategy/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,12 @@
+package lk.ijse;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-05-09
+ * @since ${VERSION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
