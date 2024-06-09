6b928486ac2b24e07f2a48bf9a9004d81ca5cebc
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-06-09 11:46:14 +0530

    implement gateway

--- micro-service/api-gateway/src/main/java/lk/ijse/api_gateway/ApiGatewayApplication.java
+++ micro-service/api-gateway/src/main/java/lk/ijse/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,11 @@ package lk.ijse.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
