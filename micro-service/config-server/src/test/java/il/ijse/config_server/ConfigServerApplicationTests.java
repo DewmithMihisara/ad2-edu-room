515db25b3b0fb3db1fe1338647d416ca5fe843ec
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-06-09 11:55:00 +0530

    add config sever

---
+++ micro-service/config-server/src/test/java/il/ijse/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package il.ijse.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
