8b285c6c7edab550812c8ec951f6be0274a911f3
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-06-09 09:51:05 +0530

    push

--- micro-service/customer-service/src/main/java/com/example/customer_service/api/CustomerController.java
+++ micro-service/customer-service/src/main/java/com/example/customer_service/api/CustomerController.java
@@ -1,8 +1,11 @@
 package com.example.customer_service.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Dewmith Mihisara
@@ -12,8 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    private final RestTemplate restTemplate;
+
+    public CustomerController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/get")
     public String get() {
         return "Hello from Customer Service";
     }
+
+    @GetMapping("/getItemInCustomer")
+    public String getItemInCustomer() {
+        return restTemplate.getForObject("http://localhost:8081/api/v1/item/get", String.class);
+    }
+
+
+    @PostMapping("/postItemInCustomer")
+    public String post() {
+        return "Hello from Customer Service";
+    }
+
 }
--- micro-service/item-service/src/main/java/com/example/item_service/api/ItemController.java
+++ micro-service/item-service/src/main/java/com/example/item_service/api/ItemController.java
@@ -1,8 +1,11 @@
 package com.example.item_service.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Dewmith Mihisara
@@ -12,8 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/item")
 public class ItemController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/get")
     public String get() {
         return "Hello from Item Service";
+    }
+
+    @GetMapping("/postCustomerInItem")
+    public String post() {
+        return restTemplate.postForObject("http://localhost:8082/api/v1/customer/postItemInCustomer", "", String.class);
     }
 }
